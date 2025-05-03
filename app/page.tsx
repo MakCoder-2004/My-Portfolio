afd5b80484d0c67359980be5378c6f5ceb53a974
Makrious-Ayman <maaakrious2004@gmail.com>
2025-05-04 02:04:34 +0300

    Custom Scrollbar is done

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
 import { Navbar } from "@/components/Navbar";
 import Tape from "@/components/Tape";
+import CustomScrollbar from "@/components/ui/CustomScrollbar";
 import { NavbarItem } from "@/interfaces";
 import { cn } from "@/lib/utils";
 import React from "react";
@@ -29,28 +30,30 @@ const navItems: NavbarItem[] = [
 export default function Home() {
   return (
     <main className="min-h-screen overflow-hidden">
-      {/* Background grid */}
-      <div>
-        <div
-          className={cn(
-            "fixed inset-0 -z-10 dark:opacity-30",
-            "[background-size:50px_50px]",
-            "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-            "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
-            "animate-grid-pulse"
-          )}
-        />
+      <CustomScrollbar style={{ overflowX: "hidden" }}>
+        {/* Background grid */}
+        <div>
+          <div
+            className={cn(
+              "fixed inset-0 -z-10 dark:opacity-30",
+              "[background-size:50px_50px]",
+              "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+              "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+              "animate-grid-pulse"
+            )}
+          />
 
-        {/* Radial gradient overlay */}
-        <div className="fixed inset-0 -z-10 pointer-events-none flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-main"></div>
-      </div>
+          {/* Radial gradient overlay */}
+          <div className="fixed inset-0 -z-10 pointer-events-none flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-main"></div>
+        </div>
 
-      {/* Content container */}
-      <Navbar navItems={navItems} />
-      <Hero />
-      <About />
-      <Tape />
-      <Contact/>
+        {/* Content container */}
+        <Navbar navItems={navItems} />
+        <Hero />
+        <About />
+        <Tape />
+        <Contact />
+      </CustomScrollbar>
     </main>
   );
 }
